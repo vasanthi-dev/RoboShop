ebba132f789bb5bd260868e4dd6a44cb1747dbbf
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-24 15:35:39 -0400

    mes

--- components/commons.sh
+++ components/commons.sh
@@ -2,7 +2,7 @@
 
 git pull
 print(){
-  echo -n -e "\e[1;32m$1\e[0m ...."
+  echo -n -e "\e[1;32m$1\e[0m .... "
   echo -e "\n\e[33m=============$1===============\e[0m" >>$LOG
   if [ $? -eq 0 ]; then
     echo -e "\e[1;32mSUCCESS\e[0m"
