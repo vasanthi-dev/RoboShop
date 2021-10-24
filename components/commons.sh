9299e6b136179c226632dca16a4121a81dc948ab
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-24 15:40:35 -0400

    messa

--- components/commons.sh
+++ components/commons.sh
@@ -4,7 +4,9 @@ git pull
 print(){
   echo -n -e "\e[1;32m$1\e[0m .... "
   echo -e "\n\e[33m=============$1===============\e[0m" >>$LOG
-  if [ $? -eq 0 ]; then
+}
+stat(){
+   if [ $1 -eq 0 ]; then
     echo -e "\e[1;32mSUCCESS\e[0m"
   else
     echo -e "\e[1;31mFAILURE\e[0m"
