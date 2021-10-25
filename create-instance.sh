3169cee870058d34ecdee576d7b78102e39ac967
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-25 19:44:31 -0400

    getting dns name and ipaddress

--- create-instance.sh
+++ create-instance.sh
@@ -8,3 +8,6 @@ if [ $COUNT -eq 0 ]
 else
   echo -e  "\e[31mInstance already exists\e[0m"
 fi
+
+IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
+sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
