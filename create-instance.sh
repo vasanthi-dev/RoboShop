35504a3265ac6a66a0e795e337ee485fac50f7f7
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-25 03:21:37 -0400

    ec2 instance creation

--- create-instance.sh
+++ create-instance.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+COUNT =$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" |jq ".Reservations[].Instances[].InstanceId" | wc -l)
+
+if [$COUNT -eq 0]
+ then
+  aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+else
+  echo -e  "\e[31mInstance already exists\e[0m"
+fi
