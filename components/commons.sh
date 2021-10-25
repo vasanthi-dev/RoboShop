6e2a11c1fe987230bc59e4d8729a0d8e67141d8b
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-24 21:20:23 -0400

    new line added -n

--- components/commons.sh
+++ components/commons.sh
@@ -3,7 +3,7 @@
 echo -e "\e[1;32m=============GIT PULL=============\e[0m"
 git pull &>/dev/null
 print(){
-  echo -e "\e[1;32m$1\e[0m ... "
+  echo -n -e "\e[1;32m$1\e[0m ... "
   echo -e "\n\e[33m=============$1===============\e[0m" >>$LOG
 }
 stat(){
