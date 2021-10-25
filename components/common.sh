a591bb335b2f99644ea8163e9d1a22dfed969f48
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-25 00:49:01 -0400

    remove git pull

--- components/common.sh
+++ components/common.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo -e "\e[1;32m=============GIT PULL=============\e[0m"
-git pull &>/dev/null
 print(){
   echo -n -e "\e[1;32m$1\e[0m ... "
   echo -e "\n\e[33m=============$1===============\e[0m" >>$LOG
