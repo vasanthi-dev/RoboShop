cc5d1c6f032fd97c080613931c47946792ed5d09
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-25 04:01:17 -0400

    added security id

--- create-instance.sh
+++ create-instance.sh
@@ -4,7 +4,7 @@ COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Re
 
 if [ $COUNT -eq 0 ]
  then
-  aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+  aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --security-group-ids sg-0b088a90c7082b15c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
 else
   echo -e  "\e[31mInstance already exists\e[0m"
 fi
