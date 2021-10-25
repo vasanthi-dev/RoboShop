137fdcf9dcd60751adc5695a5be2e43f8db1e23e
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-24 20:42:13 -0400

    failure message

--- components/commons.sh
+++ components/commons.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo -e "\e[1;32m=============GIT PULL=============\e[0m"
 git pull
 print(){
   echo -n -e "\e[1;32m$1\e[0m .... "
@@ -10,6 +10,8 @@ stat(){
     echo -e "\e[1;32mSUCCESS\e[0m"
   else
     echo -e "\e[1;31mFAILURE\e[0m"
+    echo -e "\e[1;31mScript Failed and check the detailed log in $LOG file\e[0m"
+    exit 1
   fi
 }
 LOG=/tmp/roboshop.log
