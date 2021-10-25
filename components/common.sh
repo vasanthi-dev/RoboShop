2be0a34f2907e6b6ff25d68edd034f6b8cea1a0f
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-25 00:14:05 -0400

    new common file added

---
+++ components/common.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "\e[1;32m=============GIT PULL=============\e[0m"
+git pull &>/dev/null
+print(){
+  echo -n -e "\e[1;32m$1\e[0m ... "
+  echo -e "\n\e[33m=============$1===============\e[0m" >>$LOG
+}
+stat(){
+   if [ $1 -eq 0 ]; then
+    echo -e "\e[1;32mSUCCESS\e[0m"
+  else
+    echo -e "\e[1;31mFAILURE\e[0m"
+    echo -e "\e[1;31mScript Failed and check the detailed log in $LOG file\e[0m"
+    exit 1
+  fi
+}
+LOG=/tmp/roboshop.log
+rm -f $LOG
