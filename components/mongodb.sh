6027714fdb5b20e4453315fbd5bfe0d25d163fc0
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-25 00:28:43 -0400

    downloding reposi

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source components/common.sh
+
 print "Downloading Repos"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
 stat $?
