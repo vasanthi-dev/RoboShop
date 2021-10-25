5d6852b335b8bb6a7df6b25905902d089844e956
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-25 04:37:26 -0400

    steps completed

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -6,3 +6,34 @@ print "Downloading Repos"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
 stat $?
 
+print "Install MongoDB"
+yum install -y mongodb-org &>>$Log
+stat $?
+
+print "Update MongoDB Config"
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongodb.conf &>>$LOG
+stat $?
+
+print "Enable MongoDB"
+systemctl enable mongod &>>$LOG
+stat $?
+
+print "Restart MongoDB"
+systemctl restart mongod &>>$LOG
+stat $?
+
+print "Download Schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG
+stat $?
+
+print "Extract Schema"
+unzip -o -d /tmp mongodb.zip &>>$LOG
+stat $?
+
+print "Load Schema"
+cd /tmp/mongodb-main
+mongo < catalogue.js &>>$LOG
+mongo < users.js &>>$LOG
+
+
+
