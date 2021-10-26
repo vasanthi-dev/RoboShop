5f0e43fae9fac219c527fe218b5dc8e79eea7165
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-26 19:36:06 -0400

    Change default password for mysql

--- components/mysql.sh
+++ components/mysql.sh
@@ -27,7 +27,7 @@ stat $?
 DEFAULT_PASSWORD=$(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 NEW_PASSWORD="RoboShop@1"
 
-echo -e"\e[1;32mshow databases;\e[0m" | mysql -uroot -p"${NEW_PASSWORD}" &>>$LOG
+echo 'show databases;' | mysql -uroot -p"${NEW_PASSWORD}" &>>$LOG
 if [ $? -ne 0 ]; then
   print "Changing the Default Password"
   echo -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${NEW_PASSWORD}';\nuninstall plugin validate_password;" >/tmp/pass.sql
