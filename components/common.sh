49317f7f17737d8ad7d0593dd181336837b0ec2a
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-26 02:16:28 -0400

    cart steps db check

--- components/common.sh
+++ components/common.sh
@@ -63,7 +63,7 @@ NODEJS(){
   stat $?
 
   print "Update DNS records in systemd config"
-  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
   stat $?
 
   print "Copy systemD file"
