508b62cff3ba3b026d0428cfb57431fc5a87cc86
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-25 20:10:18 -0400

    created DNS record

--- create-instance.sh
+++ create-instance.sh
@@ -10,4 +10,8 @@ else
 fi
 
 IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | xargs )
+#xargs is used to remove double quotes
 sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+
+aws route53 change-resource-record-sets --hosted-zone-id Z009674929X25UC88DYV9 --change-batch file:///tmp/record.json
+
