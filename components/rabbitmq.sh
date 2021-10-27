2ed1e4764cc06a19e109ea3697943868c85cdc93
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-27 01:19:57 -0400

    steps for rabbitmq

--- components/rabbitmq.sh
+++ components/rabbitmq.sh
@@ -8,7 +8,10 @@ COMPONENT_NAME=RabbitMQ
 COMPONENT=rabbitmq
 
 print "Install Erlang"
+yum list installed | grep erlang &>>$LOG
+if [ $? -ne 0 ]; then
 yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>$LOG
+fi
 stat $?
 print "YUM repositories for RabbitMQ"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG
