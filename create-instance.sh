5a128f345d27681c9bb20d8afd2048a93eb4a7f0
vasanthi-dev <vasanthi2dev@gmail.com>
2021-11-01 21:06:22 -0400

    spot inst

--- create-instance.sh
+++ create-instance.sh
@@ -5,7 +5,7 @@ CREATE(){
 
   if [ $COUNT -eq 0 ]
    then
-    aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t2.micro --security-group-ids sg-0b088a90c7082b15c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+    aws ec2 run-instances --image-id ami-0dc863062bc04e1de --instance-type t2.micro --security-group-ids sg-0b088a90c7082b15c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
   else
     echo -e  "\e[31mInstance already exists\e[0m"
   fi
